fcef9ee5f84a49cd4e0e9ecd224dbd2ac3a549ec
yakis <fqjxxlyq@163.com>
2020-11-23 16:24:52 +0800

    第四次提交

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,4 @@
+'''
 from django.db import models
 
 class Reporter(models.Model):
@@ -11,6 +12,31 @@ class Article(models.Model):
     headline = models.CharField(max_length=200)
     content = models.TextField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+    '''
+from django.db import models
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=70)
+    #age = models.IntegerField()
+    class Sex(models.IntegerChoices):
+        MALE = 1, '男'
+        FEMALE = 2, '女'
+        OTHER = 3, '其他'
+
+    sex = models.IntegerField(choices=Sex.choices)
+
+    def __str__(self):
+        return self.full_name
+
+class Homework(models.Model):
+    commit_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    attach = models.FileField()
+    remark = models.TextField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.headline
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,4 @@
+'''
 from django.shortcuts import render
 
 from .models import Article
@@ -6,3 +7,13 @@ def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
     context = {'year': year, 'article_list': a_list}
     return render(request, 'news/year_archive.html', context)
+'''
+
+from .models import Student,Homework
+from django.views.generic.edit import CreateView
+
+
+class HomeworkCreate(CreateView):
+    model = Homework
+    template_name = 'homework_form.html'
+    fields = ['commit_date','headline','attach','remark', 'student']
