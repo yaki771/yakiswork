b8ab359dc310d27235fd08e3e27fd7b2d0c01935
yakis <fqjxxlyq@163.com>
2020-12-27 16:31:23 +0800

    修改属性

--- mysite/yunpan/models.py
+++ mysite/yunpan/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Name(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
+
+class Article(models.Model):
+    离校日期 = models.DateField()
+    目的地 = models.CharField(max_length=200)
+    交通工具 = models.TextField()
+    姓名 = models.ForeignKey(Name, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
